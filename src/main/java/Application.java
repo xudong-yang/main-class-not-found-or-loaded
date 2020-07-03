c78f0e605848cd4a88bf8737f4bd78d77560f8ec
Xudong Yang <xudong.yang@thoughtworks.com>
2020-07-03 17:47:12 +0800

    initial commit

---
+++ src/main/java/Application.java
@@ -0,0 +1,16 @@
+import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
+
+public class Application {
+
+    public static final int PORT = 8002;
+    public static final String MOCK_DIR = "src/main/resources/mock";
+
+    public static void main(String[] args) {
+        final WireMockConfiguration configuration = WireMockConfiguration.wireMockConfig()
+                .port(PORT)
+                .usingFilesUnderDirectory(MOCK_DIR);
+        final WireMockServer server = new WireMockServer(configuration);
+        server.start();
+    }
+}
